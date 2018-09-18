c9f057c444c91bfd1524afa970d3c8c73d62fd45
Tad <Tad@cherryplaid.com>
2018-09-17 21:10:30 -0400

    Added snake class

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 // Declaring Constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const int PLAYER_START_SPEED = 2;
 
 //Setting Pointer to NULL
 SDL_Window* window = NULL;
@@ -24,33 +25,73 @@ bool init(){
 }
 
 void close(){
-    //Deallocate surface
     SDL_FreeSurface( screenSurface );
-    screenSurface = NULL;
-
-    //Destroy window
     SDL_DestroyWindow( window );
+    screenSurface = NULL;
     window = NULL;
 
-    //Quit SDL subsystems
     SDL_Quit();
 }
+
+class Snake{
+    
+    public:
+        int length, speed, x, y;
+
+        void up(){
+            y = y - speed;
+        }
+
+        void down(){
+            y = y + speed;
+        }
+
+        void right(){
+            x = x - speed;
+        }
+
+        void left(){
+            x = x + speed;
+        }
+
+        void grow(){
+            length++;
+        }
+
+        void speedUp(){
+            speed++;
+        }
+    
+};
+
+class food{
+
+};
 
 int main( int argc, char* args[] ){
     bool quit = false;
     SDL_Event e;
+    Snake player;
+    player.speed = PLAYER_START_SPEED;
     if(!init()){
-        printf("Window could not initialized!, Error: %s\n", SDL_GetError());    
+        printf("Window could not initialized! Error: %s\n", SDL_GetError());    
     }
     else {
         while(!quit){
+            SDL_PumpEvents();
+            const Uint8 *keypressed = SDL_GetKeyboardState(NULL);
+
             while( SDL_PollEvent( &e ) != 0 ) { 
-            //User requests quit 
                 if( e.type == SDL_QUIT ) { 
                     quit = true; 
                 } 
             }
-            //Get window surface 
+
+            if (keypressed[SDL_SCANCODE_A] || keypressed[SDL_SCANCODE_UP]){
+                player.up();
+                printf("X: %d, Y: %d \n", player.x, player.y );
+
+            }
             screenSurface = SDL_GetWindowSurface( window );
             SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
             SDL_UpdateWindowSurface( window );
