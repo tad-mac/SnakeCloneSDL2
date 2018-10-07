ee0ce8cbb9b09263ed2909ff96537c43aac34a93
Tad <Tad@cherryplaid.com>
2018-10-06 21:10:39 -0400

    Changed from blitting to renderer

--- main.cpp
+++ main.cpp
@@ -5,9 +5,15 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int PLAYER_START_SPEED = 2;
+const int PLAYER_HEAD_WIDTH = 40;
+const int PLAYER_HEAD_HEIGHT = 40;
+const int PLAYER_START_X = 30;
+const int PLAYER_START_Y = 30;
+
 
 //Setting Pointer to NULL
 SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
 SDL_Surface* screenSurface = NULL;
 
 //loading sub systems
@@ -20,6 +26,11 @@ bool init(){
     else {
         window = SDL_CreateWindow("SNAKE CLONE", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL){
+        success = false;
+    }
+
 
     return success;
 }
@@ -33,25 +44,54 @@ void close(){
     SDL_Quit();
 }
 
+void fillBackground(){
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+    SDL_RenderFillRect(renderer, NULL);
+}
+
 class Snake{
     
+
     public:
+        SDL_Rect src;
+        SDL_Rect head;
+        SDL_Surface* surface;
         int length, speed, x, y;
+        
+        
+        void make(){
+
+            src.x = 0;
+            src.y = 0;
+            src.w = PLAYER_HEAD_WIDTH;
+            src.h = PLAYER_HEAD_HEIGHT;
+
+            head.x = PLAYER_START_X;
+            head.y = PLAYER_START_Y;
+            head.w = PLAYER_HEAD_WIDTH;
+            head.h = PLAYER_HEAD_HEIGHT; 
+            speed = PLAYER_START_SPEED;
+        }
 
         void up(){
             y = y - speed;
+            head.y = y;
         }
 
         void down(){
             y = y + speed;
+            head.y = y;
         }
 
         void right(){
             x = x - speed;
+            head.x = x;
         }
 
         void left(){
             x = x + speed;
+            head.x = x;
         }
 
         void grow(){
@@ -72,7 +112,8 @@ int main( int argc, char* args[] ){
     bool quit = false;
     SDL_Event e;
     Snake player;
-    player.speed = PLAYER_START_SPEED;
+    player.make();
+    
     if(!init()){
         printf("Window could not initialized! Error: %s\n", SDL_GetError());    
     }
@@ -87,18 +128,27 @@ int main( int argc, char* args[] ){
                 } 
             }
 
+            if (keypressed[SDL_SCANCODE_ESCAPE]){
+                quit = true; 
+
+            }            
+
             if (keypressed[SDL_SCANCODE_A] || keypressed[SDL_SCANCODE_UP]){
                 player.up();
                 printf("X: %d, Y: %d \n", player.x, player.y );
 
             }
-            screenSurface = SDL_GetWindowSurface( window );
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            SDL_UpdateWindowSurface( window );
+
+            // screenSurface = SDL_GetWindowSurface( window );
+            // SDL_FillRect(screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            // SDL_FillRect(player.surface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0x00 ) );
+            // SDL_BlitSurface(player.surface, &player.src, screenSurface, &player.head);
+            // SDL_UpdateWindowSurface( window );
+            fillBackground();
+            SDL_RenderPresent(renderer);
         }
     }
-
+    
     close();
-
     return 0;
 }
